767d8202eb76ba210d6759307bd3fbe27444d780
Buluma <alberbuluma76@gmail.com>
2024-11-06 18:28:55 +0300

    checking servers

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
+from requests.exceptions import HTTPError
 import json
+import pandas as pd
 
 #features  ['car_data', ]
 
@@ -18,11 +20,29 @@ team_radio_data_url = 'https://api.openf1.org/v1/team_radio'
 weather_data_url = 'https://api.openf1.org/v1/weather'
 
 urls = [car_data_url, driver_data_url, intervals_data_url, laps_data_url, location_data_url, 
-       meetings_data_url, pit_data_url, position_data_url, race_control_url, sessions_data_url, 
-       pit_data_url, position_data_url, race_control_url, sessions_data_url, stints_data_url,
-       team_radio_data_url, weather_data_url]
+        meetings_data_url, pit_data_url, position_data_url, race_control_url, sessions_data_url, 
+        position_data_url, race_control_url, sessions_data_url, stints_data_url,
+        team_radio_data_url, weather_data_url]
+
 
 for url in urls:
-    response = urlopen(url)
-    data = json.loads(response.read().decode('utf-8'))
-    print(data)
+    try:
+        response = urlopen(url)
+        print(f'File {url} response successful')
+        data = json.loads(response.read().decode('utf-8'))
+
+        # Create DataFrame and print memory usage details
+        df = pd.DataFrame(data)
+        print(f"DataFrame shape: {df.shape}")
+        print(f"Memory usage: {df.memory_usage(deep=True).sum() / (1024 ** 2):.2f} MB") # memory size in mbs
+
+    except HTTPError as http_err:
+        if http_err.code == 500:
+            print(f"HTTP 500 Error for {url}. Skipping this URL.")
+        else:
+            print(f"HTTP error {http_err.code} for {url}: {http_err}")
+        # Continue to the next URL in the list
+        continue
+    except Exception as err:
+        print(f"Other error occurred for {url}: {err}")
+        continue
